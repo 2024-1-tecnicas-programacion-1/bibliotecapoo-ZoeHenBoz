2aaf564d90a52f7d4bec5e0dde4318c90ceaa6bd
ZoeHenBoz <zoehenboz20@gmail.com>
2024-04-29 23:11:27 -0500

    Taller 03 Zoe Henao Boza

--- src/main/java/com/mycompany/bibliotecapoo/Libro.java
+++ src/main/java/com/mycompany/bibliotecapoo/Libro.java
@@ -1,5 +1,57 @@
 package com.mycompany.bibliotecapoo;
 
 public class Libro {
-    // TODO: Aquí va tu código
+    private String titulo;//O(1)
+    private String autor;//O(1)
+    private int anioPubli;//O(1)
+    private String genero;//O(1)
+    private boolean leido;//O(1)
+
+    //Complejidad temporal: O(1) Tiempo constante.
+    public Libro(String titulo, String autor, int anioPubli, String genero, boolean leido) {
+        this.titulo = titulo;//O(1)
+        this.autor = autor;//O(1)
+        this.anioPubli = anioPubli;//O(1)
+        this.genero = genero;//O(1)
+        this.leido = false;//O(1)
+    }
+
+    //Complejidad temporal: O(1) Tiempo constante.
+    public String mostrarInformacion() {
+        return "Titulo: " + titulo + ", Autor: " + autor + ", Año de publicación: " + anioPubli + ", Género: " + genero;//O(1)
+    }
+
+    //Complejidad temporal: O(1) Tiempo constante.
+    public void marcarLeido() {
+        leido = true;//O(1)
+    }
+
+    //Complejidad temporal: O(1) Tiempo constante.
+    public boolean isLeido() {
+        return leido;//O(1)
+    }
+
+    //Complejidad temporal: O(1) Tiempo constante.
+    public boolean esAntiguo() {
+        if (anioPubli < 1974) {
+            return true;
+        }//O(1)
+        return false;//O(1)
+    }
+
+    //Complejidad temporal: O(1) Tiempo constante.
+    public String getTitulo() {
+        return titulo;
+    }
+
+    //Complejidad temporal: O(1) Tiempo constante.
+    public String getAutor() {
+        return autor;
+    }
+
+    //Complejidad temporal: O(1) Tiempo constante.
+    public String getGenero() {
+        return genero;
+
+    }
 }
--- src/main/java/com/mycompany/bibliotecapoo/Principal.java
+++ src/main/java/com/mycompany/bibliotecapoo/Principal.java
@@ -1,6 +1,81 @@
 package com.mycompany.bibliotecapoo;
 
+import java.util.Scanner;
+
 
 public class Principal {
-    // TODO: Aquí va tu código
+    
+    //Complejidad temporal: O(1) Tiempo constante
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Biblioteca miBiblioteca = new Biblioteca();
+
+        boolean continuar = true;
+
+        do {
+            System.out.println("Menu: ");
+            System.out.println("1) Ingresar libro");
+            System.out.println("2) Mostrar todos los libros");
+            System.out.println("3) Buscar libro");
+            System.out.println("4) Marcar libro como leído");
+            System.out.println("5) Mostrar libros no leídos");
+            System.out.println("6)Salir");
+            System.out.println("Ingrese una opcion: ");
+
+            int opcion = sc.nextInt();
+            sc.nextLine();
+            
+            switch (opcion) {
+                case 1:
+                    System.out.println("Titulo del libro: ");//O(1)
+                    String titulo = sc.nextLine();//O(1)
+                    System.out.println("Autor: ");//O(1)
+                    String autor = sc.nextLine();//O(1)
+                    System.out.println("Genero literario: ");//O(1)
+                    String genero = sc.nextLine();//O(1)
+                    System.out.println("Año de publicación: ");//O(1)
+                    int anioPubli = sc.nextInt();//O(1)
+
+                    boolean leido = false;//O(1)
+
+                    Libro libroUsuario = new Libro(titulo, autor, anioPubli, genero, leido);
+                    miBiblioteca.registrarLibro(libroUsuario);
+                    break;
+                case 2:
+                    miBiblioteca.mostrarLibros();
+
+                    break;
+                case 3:
+                    System.out.println("Ingrese su palabra de busqueda");
+                    String busqueda = sc.nextLine();
+                    miBiblioteca.buscarLibro(busqueda);
+                    break;
+                case 4:
+                    System.out.println("Ingrese el titulo del libro que desea marcar como leido");
+                    String tituloMarcado = sc.nextLine();
+
+                    Libro libroAMarcar = miBiblioteca.buscarLibro(tituloMarcado);
+
+                    if (libroAMarcar != null) {
+                        libroAMarcar.marcarLeido();
+                        System.out.println("El libro " + tituloMarcado + " se ha marcado correctamente como leido.");
+                    } else {
+                        System.out.println("No se encontro el libro " + tituloMarcado + ".");
+                    }
+                    break;
+                case 5:
+                    miBiblioteca.mostrarLibrosNoLeidos();
+                    break;
+                case 6:
+                    continuar = false;
+                    System.out.println("Gracias por usar nuestro programa, que tenga un buen dia.");
+
+                    break;
+                default:
+                    System.out.println("Opcion no valida");
+            }
+        } while (continuar);
+        sc.close();
+    }
+
 }
